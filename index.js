95043cde54d64eb4778590139ab2f17c5c5ebad0
Sin13 <65550363+Sin13@users.noreply.github.com>
2020-09-17 14:12:33 +0000

    final v1.1

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 
+if ($( window ).width() <= 990) {
+  $(".footer-row div:last-child").removeClass("align-right");
+  $(".intro").addClass("order-last");
+  $(".intro-figures").addClass("order-first");
+  $("nav .button").css("display","none");
+}
+
 $(window).on("resize", function(){
   if ($( window ).width() <= 990) {
-    console.log($( window ).width());
-    $("body").css("backgroundColor","blue");
-  } else $("body").css("backgroundColor","red");
-})
+    $(".footer-row div:last-child").removeClass("align-right");
+    $(".intro").addClass("order-last");
+    $(".intro-figures").addClass("order-first");
+    $("nav .button").css("display","none");
+  } else {
+    $(".footer-row div:last-child").addClass("align-right");
+    $(".intro").removeClass("order-last");
+    $(".intro-figures").removeClass("order-first");
+    $("nav .button").css("display","inline-block");
+}});
